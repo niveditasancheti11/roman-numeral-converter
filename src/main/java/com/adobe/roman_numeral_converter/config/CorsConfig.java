3d85f74223cd6ef85437caf249c437ea70423490
Nivedita Sancheti <niveditasancheti@macbookpro.lan>
2025-01-26 17:22:08 -0800

    added cors

---
+++ src/main/java/com/adobe/roman_numeral_converter/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.adobe.roman_numeral_converter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("http://localhost:3000") // Allow requests from React app
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // Allow cookies
+            }
+        };
+    }
+}
